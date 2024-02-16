8d994a1cf8ce7d382a262dbc959c0fbbf2296a0d
historian <pol2014dima2005@gmail.com>
2024-02-16 22:47:58 +0300

    Initial Commit

---
+++ character data/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  character data
+//
+//  Created by Dmitriy Boroda on 2/16/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
