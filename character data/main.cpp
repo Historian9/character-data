736e646d66129c98fea889e6ee2a04ddfdb160af
historian <pol2014dima2005@gmail.com>
2024-02-20 13:03:28 +0300

    создание вектора из слов строки

--- character data/main.cpp
+++ character data/main.cpp
@@ -1,25 +1,43 @@
 #include <iostream>
-#include <fstream>
-#include <string>
+#include <fstream> // для работы с файлом
+#include <string> // для работы со строками
+#include <sstream> // для создания потока ввода из строки
+#include <vector> // для создания вектора
 using namespace std;
 
-const string fileName = "/Users/dmitriyboroda/Desktop/dev/laboratory work/character data/character data/file.txt";
+const string fileName = "/Users/dmitriyboroda/Desktop/dev/laboratory work/character data/character data/file.txt"; // путь к файлу
 
 int main() {
-    ifstream inputFile(fileName);
+    ifstream inputFile(fileName); // для чтения данных из файла
 
-    if (!inputFile.is_open()) {
+    if (!inputFile.is_open()) { // проверка на наличие файла
         cout << "Файла <" << fileName << "> не существует" << endl;
         return 1;
     }
 
     string line1, line2;
+    
+    // чтение строк из файла
     getline(inputFile, line1);
     getline(inputFile, line2);
     
-    inputFile.close();
+    inputFile.close(); // закрытие файла
 
-    string fullLine = line1 + line2;
+    string fullLine = line1 + " " + line2; // создание общей строки
+    
+    istringstream iss(fullLine); // создание потока ввода из строки
+    
+    vector<string> words; 
+    
+    // создание вектора из слов строки
+    string word;
+    while (iss >> word) {
+        words.push_back(word);
+    }
+    
+//    for (int i = 0; i < words.size(); i++) {
+//        cout << words[i] << " ";
+//    }
 
     
     
