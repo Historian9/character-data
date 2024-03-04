d274609588f966ea716d0fde6e7d090723eab23a
historian <pol2014dima2005@gmail.com>
2024-03-04 19:03:09 +0300

    без библиотек

--- character data/main.cpp
+++ character data/main.cpp
@@ -1,86 +1,105 @@
 #include <iostream>
 #include <fstream> // для работы с файлом
-#include <string> // для работы со строками
-#include <sstream> // для создания потока ввода из строки
-#include <vector> // для создания вектора
-#include <unordered_map> // для использования хеш-таблицы
 using namespace std;
-//мой путь /Users/Rava/Documents/character-data/character data/file.txt
-//твой путь /Users/dmitriyboroda/Desktop/dev/laboratory work/character data/character data/file.txt
-const string fileName = "/Users/dmitriyboroda/Desktop/dev/laboratory work/character data/character data/file.txt"; // путь к файлу
 
+// путь 1 - /Users/Rava/Documents/character-data/character data/file.txt
+// путь 2 - /Users/dmitriyboroda/Desktop/dev/laboratory work/character data/character data/file.txt
+const char* fileName = "/Users/dmitriyboroda/Desktop/dev/laboratory work/character data/character data/file.txt"; // путь к файлу
 
 // функция для подсчета повторений символов в слове
-bool hasDuplicateLetters(const string& word){
-    unordered_map<char, int> letterCount; // создания хеш-таблицы (словаря), где будут храниться буквы слова, и сколько раз они встречаются
-    
-    for (int i = 0; i < word.size(); i++){
-        letterCount[word[i]]++;
-        if (letterCount[word[i]] > 1){
+bool hasDuplicateLetters(const char* word, int length) {
+    int letterCount[256] = {0}; // массив для подсчета повторений символов (ASCII)
+
+    for (int i = 0; i < length; i++) {
+        letterCount[(unsigned int)word[i]]++; // делаем из символа ASCII номер и прибавляем 1 к элементу массива с этим индексом
+        if (letterCount[(unsigned int)word[i]] > 1) { // если этот элемент > 1, то этот символ уже встречался в слове
             return false; // найдено повторение
         }
     }
     return true; // нет повторений
 }
 
-
 int main() {
     ifstream inputFile(fileName); // для чтения данных из файла
 
-    if (!inputFile.is_open()){ // проверка на наличие файла
+    if (!inputFile.is_open()) { // проверка на наличие файла
         cout << "Файла <" << fileName << "> не существует" << endl;
         return 1;
     }
-    
-    string line1, line2;
-    
-    getline(inputFile, line1); // чтение первой строки из файла
-    
-    if (line1 == ""){ // проверка на наличие элементов в файле
-        cout << "Файл пустой";
+
+    char line1[256], line2[256];
+
+    inputFile.getline(line1, sizeof(line1)); // чтение первой строки из файла
+
+    if (line1[0] == '\0') { // проверка на наличие элементов в файле (1 строка)
+        cout << "1 строка пустая";
         return 2;
     }
-    
-    getline(inputFile, line2); // чтение второй строки из файла
+
+    inputFile.getline(line2, sizeof(line2)); // чтение второй строки из файла
+
+    if (line2[0] == '\0') { // проверка на наличие элементов в файле (2 строка)
+        cout << "2 строка пустая";
+        return 3;
+    }
     
     inputFile.close(); // закрытие файла
 
-    string fullLine = line1 + " " + line2; // создание общей строки
+    char fullLine[512]; // создание общей строки
+    strcpy(fullLine, line1); // Копирование содержимого line1 в fullLine
+    strcat(fullLine, " "); // Добавление пробела к fullLine
+    strcat(fullLine, line2); // Добавление содержимого line2 к fullLine
     
-    istringstream iss(fullLine); // создание потока ввода из строки
-    
-    vector<string> words; 
-    
-    // создание вектора из слов строки
-    string word;
-    while (iss >> word){
-        words.push_back(word);
-    }
-    
-    // проходимся по вектору и проверяем слова на неповторяемость элементов
     cout << "Слова, у которых все буквы различны: ";
-    for (int i = 0; i < words.size(); i++){
-        if (hasDuplicateLetters(words[i])){
-            cout << words[i] << " ";
-        }
-    }
-    cout << endl;
+    int i = 0;
+    int wordStart = 0; // для обозначения первого индекса слова в строке
+    int flag = 0; // флаг для переноса wordStart
 
+    do {
+        if (flag == 1 and fullLine[i] != ' ' and fullLine[i] != '\n' and fullLine[i] != '\t' and fullLine[i] != '\0') {
+            wordStart = i; // индикатор начала слова
+            flag = 0; // обнуляем флаг
+        }
+
+        if (fullLine[i] == ' ' or fullLine[i] == '\n' or fullLine[i] == '\t' or fullLine[i] == '\0') { // если дошли до конца слова в строке
+            if (hasDuplicateLetters(fullLine + wordStart, i - wordStart)) { // проверяем слово на повторение букв
+                for (int j = wordStart; j < i; j++) {
+                    cout << fullLine[j]; // выводим слово из строки
+                }
+                cout << " ";
+            }
+            flag = 1; // чтобы перенести wordStart на начало следующего слова
+        }
+        i++;
+        
+    } while (fullLine[i - 1] != '\0'); // используем do-while, чтобы мы сначала дошли до конца строки, проверили слово на повторение букв и вывели его (i - 1 используется для того же)
+    
+    cout << endl;
+    
     cout << "Чередование символов исходной строки через два: ";
     
-    // поочередно выводим по два символа из каждой строки
-    for (int i = 0; i < min(line1.size(), line2.size()) - 1; i += 2){
-        cout << line1[i] << line1[i + 1] << line2[i] << line2[i + 1];
+    i = 0;
+    int j = 0;
+
+    // Продолжаем, пока хотя бы одна из строк не закончится
+    while (line1[i] != '\0' || line2[j] != '\0') {
+        // Выводим первые 2 символа из первой строки
+        for (int k = 0; k < 2; k++) {
+            if (line1[i] != '\0') {
+                cout << line1[i];
+                i++;
+            }
+        }
+        // Выводим первые 2 символа из второй строки
+        for (int k = 0; k < 2; k++) {
+            if (line2[j] != '\0') {
+                cout << line2[j];
+                j++;
+            }
+        }
     }
-    
-    // если символов в строках было разное количество -> выводим оставшиеся символы
-    if ((min(line1.size(), line2.size()) == line1.size()) and (line1.size() != line2.size())){
-        cout << line2.substr(line1.size() - 1);
-    } else if ((min(line1.size(), line2.size()) == line2.size()) and (line1.size() != line2.size())){
-        cout << line1.substr(line2.size() - 1);
-    }
-    
+
     cout << endl;
+
     return 0;
 }
-
